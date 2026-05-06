cd5f4ac09041b2a373d1909e457bdf3b6455274d
kane7372 <kane7372@gmail.com>
2026-05-06 10:12:27 +0900

    Update rksimapapp.py

--- rksimapapp.py
+++ rksimapapp.py
@@ -246,7 +246,7 @@ with tab1:
                 )    
     c3, c4, c5 = st.columns(3)
     with c3: 
-        c5_stats = monthly_stats[~monthly_stats['STS_Detail'].str.contains('CNL|DLA', na=False)]
+        c3_stats = monthly_stats[~monthly_stats['STS_Detail'].str.contains('CNL|DLA', na=False)]
         st.plotly_chart(px.bar(monthly_stats, x='YM', y='Avg_Total_Delay', color='STS_Detail', barmode='group', title="전체 항공편 단순 평균 지연 시간(분)"), use_container_width=True)
     with c4 : st.plotly_chart(px.bar(monthly_stats, x='YM', y='Avg_Delay_Time', color='STS_Detail', barmode='group', title="15분 이상 지연 항공편 월평균 지연 시간(분)"), use_container_width=True)
     with c5 :
